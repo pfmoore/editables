6896e0c973d465b290ddd65421c0565766934ce6
Paul Moore <p.f.moore@gmail.com>
2022-04-09 15:51:13 +0100

    Minor formatting fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'editables'
-copyright = '2021, Paul Moore'
-author = 'Paul Moore'
+project = "editables"
+copyright = "2021, Paul Moore"
+author = "Paul Moore"
 
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = "0.2"
 
 
 # -- General configuration ---------------------------------------------------
@@ -35,7 +35,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -48,9 +48,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
