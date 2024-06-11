9e6f3a6ed22183d4c693fc73fddd57b624a70d37
Jason R. Coombs <jaraco@jaraco.com>
2024-06-11 16:41:25 -0400

    Add support for redirecting for a namespace package.

--- src/editables/redirector.py
+++ src/editables/redirector.py
@@ -3,26 +3,57 @@ import importlib.machinery
 import importlib.util
 import sys
 from types import ModuleType
-from typing import Dict, Optional, Sequence, Union
+from typing import Dict, Optional, Sequence, Set, Union
 
 ModulePath = Optional[Sequence[Union[bytes, str]]]
 
 
 class RedirectingFinder(importlib.abc.MetaPathFinder):
     _redirections: Dict[str, str] = {}
+    _parents: Set[str] = set()
 
     @classmethod
     def map_module(cls, name: str, path: str) -> None:
         cls._redirections[name] = path
+        cls._parents.update(cls.parents(name))
+
+    @classmethod
+    def parents(cls, name):
+        """
+        Given a full name, generate all parents.
+
+        >>> list(RedirectingFinder.parents('a.b.c.d'))
+        ['a.b.c', 'a.b', 'a']
+        """
+        base, sep, name = name.rpartition('.')
+        if base:
+            yield base
+            yield from cls.parents(base)
 
     @classmethod
     def find_spec(
         cls, fullname: str, path: ModulePath = None, target: Optional[ModuleType] = None
     ) -> Optional[importlib.machinery.ModuleSpec]:
-        if "." in fullname:
-            return None
-        if path is not None:
-            return None
+        return cls.spec_from_parent(fullname) or cls.spec_from_redirect(fullname)
+
+    @classmethod
+    def spec_from_parent(
+        cls, fullname: str
+    ) -> Optional[importlib.machinery.ModuleSpec]:
+        if fullname in cls._parents:
+            return importlib.util.spec_from_loader(
+                fullname,
+                importlib.machinery.NamespaceLoader(
+                    fullname,
+                    path=[],
+                    path_finder=cls.find_spec,
+                ),
+            )
+
+    @classmethod
+    def spec_from_redirect(
+        cls, fullname: str
+    ) -> Optional[importlib.machinery.ModuleSpec]:
         try:
             redir = cls._redirections[fullname]
         except KeyError:
